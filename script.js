2342bc53d548dce38af08d8a7448c1da160bf17d
Saliq Tariq <56510866+saliqtariq@users.noreply.github.com>
2025-09-26 16:04:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currmode= "light";
+ 
+btn.addEventListener("click" , () => {
+  if(currmode==="light") {
+    currmode="dark";
+    body.classList.add("dark");
+    body.classList.remove("light");
+ } else {
+  currmode="light";
+  body.classList.add("light");
+  body.classList.remove("dark");
+ }
+  console.log(currmode);
+});
